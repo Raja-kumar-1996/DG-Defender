78253be3a0185a02f8946270eec02acb1cb4cd79
Raj <88266173+Raja-kumar-1996@users.noreply.github.com>
2026-08-02 08:51:36 +0530

    Implement logging functionality in logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -1,1 +1,18 @@
+import logging
+import sys
 
+LOGGER = logging.getLogger("DG-Defender")
+
+LOGGER.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+handler.setFormatter(formatter)
+
+if not LOGGER.handlers:
+    LOGGER.addHandler(handler)
