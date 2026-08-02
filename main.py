a516f97a563e4251fa9fbddf3f70a6cc98bf59e6
Raj <88266173+Raja-kumar-1996@users.noreply.github.com>
2026-08-02 07:25:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from bot import app
+from bot.handlers import *
 
+from bot.utils.logger import LOGGER
+
+
+def main():
+
+    LOGGER.info("=" * 50)
+    LOGGER.info("Starting DG Defender...")
+    LOGGER.info("=" * 50)
+
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
